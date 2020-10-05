7a9b1785b5c1051ab190bbd95499608c23db0c3f
Gonzalo Coradini <gonzalocoradini@gmail.com>
2020-10-05 17:55:22 -0300

    change random number generator

--- src/main/java/core/RandomNumber.java
+++ src/main/java/core/RandomNumber.java
@@ -8,6 +8,19 @@ import java.util.Random;
 public class RandomNumber {
 
     public static BigInteger generateBigRandomNumber(int digits){
+        SecureRandom rng = new SecureRandom();
+        BigInteger random = getBigRandomNumber(digits,rng);
+        return random;
+    }
+
+    public static BigInteger getBigRandomNumber(int digCount, Random rnd){
+        final char[] bigNumber = new char[digCount];
+        for(int i = 0; i < digCount; i++)
+            bigNumber[i] = (char) ('0' + (i == 0 ? rnd.nextInt(9) + 1 : rnd.nextInt(10)));
+        return new BigInteger(new String(bigNumber));
+    }
+
+    public static BigInteger generateBigRandomNumberFromBits(int digits){
         int bitsize = (int)(digits * 3.3);
         SecureRandom rng = new SecureRandom();
         BigInteger random = createRandomForBitSize(bitsize, rng);
